a6cfd8263c2ea05dad6a672fcce39e2fd344588b
Banura Randika <randika.banura@gmail.com>
2023-04-14 04:41:24 +0530

    Model update

--- api/src/main/java/com/lohika/morning/ml/api/controller/LyricsController.java
+++ api/src/main/java/com/lohika/morning/ml/api/controller/LyricsController.java
@@ -6,11 +6,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @RestController
 @RequestMapping("/lyrics")
 public class LyricsController {
